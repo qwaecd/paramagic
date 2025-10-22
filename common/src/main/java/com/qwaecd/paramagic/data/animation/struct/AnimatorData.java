b645b70693eeca44ef2733a44a9b3d27f396a22e
qwaecd <200468713+qwaecd@users.noreply.github.com>
2025-10-22 21:31:10 +0800

    docs(animation):为 AnimatorData 构造函数添加详细注释
    - 添加了构造函数的 JavaDoc 注释，说明使用建议- 增加了中文注释说明，提高代码可读性- 添加了对 TimelineBuilder 的引用链接
    - 明确指出 tracks 参数的含义和用途

--- common/src/main/java/com/qwaecd/paramagic/data/animation/struct/AnimatorData.java
+++ common/src/main/java/com/qwaecd/paramagic/data/animation/struct/AnimatorData.java
@@ -15,6 +15,14 @@ public class AnimatorData {
     @Getter
     private final List<TrackData<?>> tracks;
 
+    /**
+     * Constructs an AnimatorData with the given tracks.<br>
+     * If you are not very sure about what you are doing, please use TimelineBuilder to build AnimatorData.
+     * <p>
+     * 除非你非常清楚自己在做什么，否则请使用 TimelineBuilder 来构建 AnimatorData.
+     * @see com.qwaecd.paramagic.data.animation.util.TimelineBuilder
+     * @param tracks 一个包含所有轨道的列表.
+     */
     public AnimatorData(List<TrackData<?>> tracks) {
         this.tracks = tracks;
     }
