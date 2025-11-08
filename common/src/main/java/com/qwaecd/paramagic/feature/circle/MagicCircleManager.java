1acec9a90b07595a34944906969cd90c14d8a52e
qwaecd <200468713+qwaecd@users.noreply.github.com>
2025-11-08 18:15:41 +0800

    docs(circle): 更新 MagicCircleManager 注释

--- common/src/main/java/com/qwaecd/paramagic/feature/circle/MagicCircleManager.java
+++ common/src/main/java/com/qwaecd/paramagic/feature/circle/MagicCircleManager.java
@@ -44,7 +44,6 @@ public class MagicCircleManager {
     /**
      * Call this function only within the rendering loop, not in the game logic loop.<br>
      * 仅在渲染循环内调用该函数，不要在游戏逻辑循环内调用。<br>
-     * 更新粒子效果的状态，包括其所有发射器的状态。<br>
      * @param deltaTime Seconds of time increment (time since last frame).<br>
      * 时间增量，单位秒（为距离上一帧的时间）<br>
      */
