2490161e19b1f33f5e9ef3a11e607a3b46d35aa8
qwaecd <200468713+qwaecd@users.noreply.github.com>
2026-04-13 19:05:07 +0800

    添加mod图标，为引力坍缩添加粒子特效

--- common/src/main/java/com/qwaecd/paramagic/client/renderer/entity/GravityCollapseEntityRenderer.java
+++ common/src/main/java/com/qwaecd/paramagic/client/renderer/entity/GravityCollapseEntityRenderer.java
@@ -2,7 +2,9 @@ package com.qwaecd.paramagic.client.renderer.entity;
 
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.qwaecd.paramagic.tools.TimeProvider;
 import com.qwaecd.paramagic.world.entity.projectile.GravityCollapseEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.world.phys.Vec3;
@@ -17,5 +19,7 @@ public class GravityCollapseEntityRenderer extends EmptyEntityRenderer<GravityCo
         super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
         Vec3 position = entity.getPosition(partialTick);
         entity.setDistortionPosition((float) position.x, (float) position.y, (float) position.z);
+
+        entity.renderEffect(partialTick, TimeProvider.getDeltaTime(Minecraft.getInstance()));
     }
 }
